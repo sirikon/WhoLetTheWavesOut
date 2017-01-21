6142521d000058d21cccae13d2a0dbf57ca6392a
Carlos Fernández Llamas <sirikon2@gmail.com>
2017-01-21 23:59:58 +0100

    Working on lines

---
+++ Assets/Scripts/BallSpawnerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSpawnerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(CreateWaves(3));
+	}
+
+    IEnumerator CreateWaves(int waves)
+    {
+        for(var i = 0; i < waves; i++)
+        {
+            StartCoroutine(CreateWave(10));
+            yield return new WaitForSeconds(30);
+        }
+    }
+
+    IEnumerator CreateWave(int balls)
+    {
+        for(var i = 0; i < balls; i++)
+        {
+            InstantiateNewBall();
+            yield return new WaitForSeconds(0.5F);
+        }
+    }
+
+    GameObject createBall()
+    {
+        return (GameObject)Instantiate(Resources.Load("Prefabs/Ball"));
+    }
+
+    void InstantiateNewBall() {
+        var go = createBall();
+        var newPosition = Random.insideUnitSphere * 3;
+        newPosition.y += 10;
+        go.transform.position = newPosition;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
