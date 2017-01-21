841a696a4bce2f4f75b2e220e180031eb807bc48
Carlos Fernández Llamas <sirikon2@gmail.com>
2017-01-21 14:59:40 +0100

    Player physics

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -5,20 +5,25 @@ using UnityEngine;
 public class CharacterController : MonoBehaviour {
 
     MagnetismOrigin magnetismOrigin;
+    Rigidbody rb;
 
 	// Use this for initialization
 	void Start () {
         magnetismOrigin = GetComponent<MagnetismOrigin>();
+        rb = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        float z = Input.GetAxis("Vertical") * 10 * Time.deltaTime;
-        float x = Input.GetAxis("Horizontal") * 10 * Time.deltaTime;
+        float z = Input.GetAxis("Vertical 1") * 10 * Time.deltaTime;
+        float x = Input.GetAxis("Horizontal 1") * 10 * Time.deltaTime;
         Vector3 translation = new Vector3(x, 0, z);
                 
         transform.LookAt(transform.position + translation);
         transform.Translate(0, 0, translation.magnitude);
+
+        rb.velocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
 
         magnetismOrigin.State = 
             CustomInput.GetButton(CustomInputButton.PlayerAction) ? 
